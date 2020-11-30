7cb3fe7669e6678e4b090b254018038e481c920b
omer <omerfussfeld@gmail.com>
2020-11-30 22:00:05 +0200

    aa

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -3,13 +3,17 @@ module.exports = connectSockets
 
 function connectSockets(io) {
     io.on('connection', socket => {
+        socket.on('beat update', beat => {
+            console.log('**********', beat);
+            io.to(socket.myTopic).emit('beat update', beat)
+        })
+        socket.on('add song', song => {
+            io.to(socket.myTopic).emit('add song', song)
+        })
         socket.on('chat newMsg', msg => {
-            console.log(msg)
-            // io.emit('chat addMsg', msg)
-            // emits only to sockets in the same room
             io.to(socket.myTopic).emit('chat addMsg', msg)
         })
-        socket.on('chat topic', topic => {
+        socket.on('beat topic', topic => {
             if (socket.myTopic) {
                 socket.leave(socket.myTopic)
             }
@@ -17,13 +21,7 @@ function connectSockets(io) {
             socket.myTopic = topic;
         })
         socket.on('user typing', typing => {
-            console.log(typing);
             socket.broadcast.emit('typing', typing);
-            // io.emit('typing', typing)
         })
     })
 }
-                                    // socket.on('add song', song =>{
-                                    //     console.log('song',song)
-                                    //     // io.to(socket.myTopic).emit('add song',song)
-                                    // })
