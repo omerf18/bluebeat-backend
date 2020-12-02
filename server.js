f8789ba5c1b5b065b139459b220b90b79e160234
Roni Gabbay <ronigabbay8@gmail.com>
2020-12-02 14:26:26 +0200

    user2

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')));
 } else {
     const corsOptions = {
-        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3030', 'http://localhost:3030'],
+        // origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3030', 'http://localhost:3030'],
+        origin: ['http://127.0.0.1:8083', 'http://localhost:8083', 'http://127.0.0.1:3030', 'http://localhost:3030'],
         credentials: true
     };
     app.use(cors(corsOptions));
