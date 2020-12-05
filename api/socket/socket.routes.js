c51106e700de1dfa9cd4ce18e5107f53ccb33031
omer <omerfussfeld@gmail.com>
2020-12-05 13:22:42 +0200

    a

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -6,6 +6,9 @@ function connectSockets(io) {
         socket.on('sendMsg', (msg) => {
             io.to(socket.roomId).emit('sentMsg', msg)
         })
+        socket.on('sendEmoji', (emoji) => {
+            socket.broadcast.to(socket.roomId).emit('reciveEmoji', emoji)
+        })
         socket.on('beatChanged', (beat) => {
             socket.broadcast.to(socket.roomId).emit('beatChanged', beat)
         })
